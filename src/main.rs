15449515bdd5a3032006a8342d54b31ea0a7b8b2
lenovo1 <OmkarMahajan11>
2026-04-14 14:36:52 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
 #![allow(unused_imports)]
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    thread,
 };
 
-fn main() {
+use async_net::TcpListener;
+use futures_lite::io::{AsyncReadExt, AsyncWriteExt};
+
+use smol::io;
+
+fn main() -> io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment the code below to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    smol::block_on(async {
+        let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+        loop {
+            let (mut stream, _addr) = listener.accept().await?;
+
+            let _ = smol::spawn(async move {
                 let mut buf = [0; 1024];
 
                 loop {
-                    let n = match stream.read(&mut buf) {
-                        Ok(0) => break,
+                    let n = match stream.read(&mut buf).await {
+                        Ok(0) => return,
                         Ok(n) => n,
                         Err(e) => {
                             eprintln!("read error: {e}");
-                            break;
+                            return;
                         }
                     };
 
-                    if buf[..n].eq(b"*1\r\n$4\r\nPING\r\n") {
-                        _ = stream.write_all(b"+PONG\r\n");
+                    if let Err(e) = stream.write_all(b"+PONG\r\n").await {
+                        eprintln!("write error: {e}");
+                        return;
                     }
                 }
-            }
-            Err(e) => eprintln!("accept error: {e}"),
+            })
+            .detach();
         }
-    }
+    })
 }
